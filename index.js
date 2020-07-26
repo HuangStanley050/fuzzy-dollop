4fe5aac7a35d3e10e939b9ed3879857cc61fafe3
Stanley Huang <infamous_godhand@yahoo.com>
2020-07-26 11:57:18 +1000

    combined data and learned expandDims() method

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import * as tf from "@tensorflow/tfjs";
+const tf = require("@tensorflow/tfjs-node");
 
-const jumpData = tf.tensor([[], [], []]);
-const playerData = tf.tensor([], [], []);
+const jumpData = tf.tensor([
+  [70, 70, 70],
+  [70, 70, 70],
+  [70, 70, 70],
+  [70, 70, 70],
+]);
+const playerData = tf.tensor([
+  [1, 160],
+  [1, 160],
+  [1, 160],
+  [1, 160],
+]);
+
+const finalDataSet = jumpData.sum(1).expandDims(1).concat(playerData, 1);
+
+finalDataSet.print();
